c49b8a58a94a7444721cf309fe2cc4ce0fb9d5a0
Benjamin Baudel <bbaudel@gopro.com>
2017-03-28 16:41:02 +0200

    Update changelog for 0.9.2

--- src/render/RenderManager.js
+++ src/render/RenderManager.js
@@ -784,7 +784,6 @@ FORGE.RenderManager.prototype._setBackgroundRendererType = function(vrEnabled)
 
     var mediaConfig = /** @type {SceneMediaConfig} */ (this._sceneConfig.media);
 
-
     if (typeof mediaConfig.source === "undefined" || 
         mediaConfig.source.format === FORGE.MediaFormat.CUBE || 
         mediaConfig.source.format === FORGE.MediaFormat.FLAT ||
@@ -795,13 +794,11 @@ FORGE.RenderManager.prototype._setBackgroundRendererType = function(vrEnabled)
         {
             this._backgroundRendererType = FORGE.BackgroundType.SHADER;
         }
-
         else
         {
             this._backgroundRendererType = FORGE.BackgroundType.MESH;
         }
     }
-
     else
     {
         this._backgroundRendererType = FORGE.BackgroundType.SHADER;
