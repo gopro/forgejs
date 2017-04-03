6ed3e9b00b583818940650b9649e5c10928865d0
Raphaël Roux <rroux@gopro.com>
2017-04-03 11:58:34 +0200

    New externs for Camera

---
+++ externs/forge/camera.ext.js
@@ -0,0 +1,8 @@
+/**
+ * @typedef {{axis:string, min:number, max:number}}
+ * @name CameraBoundaries
+ * @property {string} axis
+ * @property {number} max
+ * @property {number} min
+ */
+var CameraBoundaries;
