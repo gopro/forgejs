d69ca2ed10edb005b2b5be133540af9cd9cf99f8
Adrien Berthet <aberthet@gopro.com>
2017-05-15 11:29:53 +0200

    Change the returned object by a THREE.Texture, and add the support of the age and decay of a texture. The default cache size is 30Mo for now, but it is completely arbitrary, we may decide to chose another value which could be more suitable.

--- src/media/MediaStore.js
+++ src/media/MediaStore.js
@@ -1,7 +1,7 @@
 /**
  * This object stores a number of tiles used for multi resolution cases with
  * tiles. It acts as a LRU map, as we can't store infinite amount of tiles.
- * The number of tiles to store is (6 * 2^n)!, with n being the number of levels.
+ * The number of tiles to store is (6 * 4^n)!, with n being the number of levels.
  *
  * There is an exception though: the level 0 of a multi resolution is always
  * kept in the cache.
@@ -31,21 +31,21 @@ FORGE.MediaStore = function(viewer, config)
     this._config = config;
 
     /**
-     * A map containing all {@link FORGE.Image}, with the key being constitued
-     * from the level, face, x and y properties defining the image
-     * @name FORGE.MediaStore#_images
+     * A map containing all {@link FORGE.MediaTexture}, with the key being constitued
+     * from the level, face, x and y properties defining the texture
+     * @name FORGE.MediaStore#_textures
      * @type {?FORGE.Map}
      * @private
      */
-    this._images = null;
+    this._textures = null;
 
     /**
-     * The maximum size of the store
-     * @name FORGE.MediaStore#_maxSize
+     * The current size of all loaded textures.
+     * @name FORGE.MediaStore#_size
      * @type {number}
      * @private
      */
-    this._maxSize = 0;
+    this._size = 0;
 
     /**
      * The pattern to follow to laod the images
@@ -64,6 +64,15 @@ FORGE.MediaStore.prototype = Object.create(FORGE.BaseObject.prototype);
 FORGE.MediaStore.prototype.constructor = FORGE.MediaStore;
 
 /**
+ * The maximum size of texture at once. It is set at 30Mb, as we assume the
+ * median size of a cache is 32Mb, and we keep 2Mb for other texture.
+ * @name FORGE.MediaStore.MAX_SIZE
+ * @type {string}
+ * @const
+ */
+FORGE.MediaStore.MAX_SIZE = 31457280;
+
+/**
  * Boot routine.
  *
  * @method FORGE.MediaStore#_boot
@@ -73,8 +82,7 @@ FORGE.MediaStore.prototype._boot = function()
 {
     this._register();
 
-    this._images = new FORGE.Map();
-    this._maxSize = 200; // can contains level 0 to 4
+    this._textures = new FORGE.Map();
 
     this._parseConfig(this._config);
 };
@@ -99,36 +107,58 @@ FORGE.MediaStore.prototype._parseConfig = function(config)
 };
 
 /**
+ * Create the key of an image.
+ *
+ * @method FORGE.MediaStore#_createKey
+ * @param {string} face - the face associated to this image
+ * @param {number=} level - the level of quality
+ * @param {number=} x - the x coordinate for position
+ * @param {number=} y - the y coordinate for position
+ * @return {string} returns the key for this image
+ * @private
+ */
+FORGE.MediaStore.prototype._createKey = function(face, level, x, y)
+{
+    var key = "";
+    key += typeof face !== "undefined" ? face + "-" : "";
+    key += typeof level !== "undefined" ? level  + "-" : "";
+    key += typeof x !== "undefined" ? x + "-" : "";
+    key += typeof y !== "undefined" ? y : "";
+
+    return key;
+};
+
+/**
  * Loads an Image from parameters, but doesn't add it to the map yet.
  *
  * @method FORGE.MediaStore#_load
+ * @param {string} key - the key of the image
  * @param {string} face - the face associated to this image
  * @param {number} level - the level of quality
  * @param {number} x - the x coordinate for position
  * @param {number} y - the y coordinate for position
  * @private
  */
-FORGE.MediaStore.prototype._load = function(face, level, x, y)
+FORGE.MediaStore.prototype._load = function(key, face, level, x, y)
 {
-    var key = "";
-    key += face;
-    key += "-" + level;
-    key += "-" + x;
-    key += "-" + y;
-
     var url = this._pattern;
-    url.replace(/{face}/, face);
-    url.replace(/{level}/, level.toString());
-    url.replace(/{x}/, x.toString());
-    url.replace(/{y}/, y.toString());
+    url = url.replace(/\{face\}/, face);
+    url = url.replace(/\{level\}/, level.toString());
+    url = url.replace(/\{x\}/, x.toString());
+    url = url.replace(/\{y\}/, y.toString());
 
     var config = {
-        key: key,
         url: url
     };
 
     var image = new FORGE.Image(this._viewer, config);
-    image.onLoadComplete.addOnce(this._onLoadComplete, this);
+
+    image.data = {
+        key: key,
+        level: level
+    };
+
+    image.onLoadComplete.add(this._onLoadComplete, this);
 };
 
 /**
@@ -140,13 +170,70 @@ FORGE.MediaStore.prototype._load = function(face, level, x, y)
  */
 FORGE.MediaStore.prototype._onLoadComplete = function(image)
 {
-    this._images.set(image.uid, image);
+    image = image.emitter;
+
+    var texture = new THREE.Texture();
+    texture.image = image.element;
+
+    var size = image.element.height * image.element.width;
+    this._size += size;
+
+    var mediaTexture = new FORGE.MediaTexture(texture, (image.data.level === 0), size);
+    this._textures.set(image.data.key, mediaTexture);
+
+    // destroy the image, it is no longer needed
+    image.destroy();
+
+    this._checkSize();
+};
+
+/**
+ * Check the current size of the store, and flush some texture if necessary.
+ *
+ * @method FORGE.MediaStore#_checkSize
+ * @private
+ */
+FORGE.MediaStore.prototype._checkSize = function()
+{
+    if (this._size < FORGE.MediaStore.MAX_SIZE)
+    {
+        return;
+    }
+
+    var entries = this._textures.entries(),
+        time = window.performance.now();
+
+    entries = FORGE.Utils.sortArrayByProperty(entries, "1.lastTime");
+
+    var force = false;
+
+    while (this._size > FORGE.MediaStore.MAX_SIZE)
+    {
+        // oldest are first
+        texture = entries.shift();
+
+        // if no  more entries (aka all texture are level 0) remove it anyway
+        if (typeof texture === "undefined")
+        {
+            entries = this._textures.entries();
+            entries = FORGE.Utils.sortArrayByProperty(entries, "1.lastTime");
+            force = true;
+        }
+
+        // but don't delete if it is locked
+        if (texture[1].locked !== true || force === true)
+        {
+            this._size -= texture[1].size;
+            texture[1].destroy();
+            this._textures.delete(texture[0]);
+        }
+    }
 };
 
 /**
  * Get an image from this store, given four parameters: the face associated to
  * this image, the level of quality and the x and y positions. It returns
- * either a {@link FORGE.Image} or null.
+ * either a {@link THREE.Texture} or null.
  *
  * The inner working is as follow: either the image is already loaded and
  * returned, or either the image is being loaded and nothing is returned yet.
@@ -158,25 +245,20 @@ FORGE.MediaStore.prototype._onLoadComplete = function(image)
  * @param {number} level - the level of quality
  * @param {number} x - the x coordinate for position
  * @param {number} y - the y coordinate for position
- * @return {?FORGE.Image} returns the image if present, else null
+ * @return {?THREE.Texture} returns the image if present, else null
  */
 FORGE.MediaStore.prototype.get = function(face, level, x, y)
 {
-    var key = "";
+    var key = this._createKey(face, level, x, y);
 
-    key += face;
-    key += "-" + level;
-    key += "-" + x;
-    key += "-" + y;
-
-    var res = /** @type {FORGE.Image} */ (this._images.get(key));
+    var res = /** @type {FORGE.MediaTexture} */ (this._textures.get(key));
 
     if (res !== undefined)
     {
-        return res;
+        return res.texture;
     }
 
-    this._load(face, level, x, y);
+    this._load(key, face, level, x, y);
 
     return null;
 };
@@ -190,6 +272,6 @@ FORGE.MediaStore.prototype.destroy = function()
 {
     this._viewer = null;
 
-    this._images.clear();
-    this._images = null;
+    this._textures.clear();
+    this._textures = null;
 };
---
+++ src/media/MediaTexture.js
@@ -0,0 +1,142 @@
+/**
+ * This object stores a THREE.Texture used for multi resolutions scene. It is
+ * simplier (in terms of memory to store this object as it is tinier than a
+ * FORGE.Image. It also remove the fact that we need to create a THREE.Texture
+ * in the renderer.
+ *
+ * @constructor FORGE.MediaTexture
+ * @param {THREE.Texture} texture - the THREE.Texture to store
+ * @param {boolean} locked - is the texture locked (i.e. it isn't deletable)
+ * @param {number} size - the size of the texture
+ */
+FORGE.MediaTexture = function(texture, locked, size)
+{
+    /**
+     * The texture
+     * @name FORGE.MediaTexture#_texture
+     * @type {THREE.Texture}
+     * @private
+     */
+    this._texture = texture;
+
+    /**
+     * Can the texture be deleted ? Otherwise it is locked, e.g. it is a level 0
+     * @name FORGE.MediaTexture#_locked
+     * @type {boolean}
+     * @private
+     */
+    this._locked = locked;
+
+    /**
+     * The size of the texture
+     * @name FORGE.MediaTexture#_size
+     * @type {number}
+     * @private
+     */
+    this._size = size;
+
+    /**
+     * The time the texture was last used
+     * @name FORGE.MediaTexture#_lastTime
+     * @type {number}
+     * @private
+     */
+    this._lastTime = window.performance.now();
+
+    /**
+     * The number of times the texture was used
+     * @name FORGE.MediaTexture#_count
+     * @type {number}
+     * @private
+     */
+    this._count = 0;
+};
+
+FORGE.MediaTexture.prototype.constructor = FORGE.MediaTexture;
+
+/**
+ * Destroy routine
+ * @method FORGE.MediaTexture#destroy
+ */
+FORGE.MediaTexture.prototype.destroy = function()
+{
+    this._texture = null;
+};
+
+/**
+ * Get the texture.
+ * @name  FORGE.MediaTexture#texture
+ * @type {string}
+ * @readonly
+ */
+Object.defineProperty(FORGE.MediaTexture.prototype, "texture",
+{
+    /** @this {FORGE.MediaTexture} */
+    get: function()
+    {
+        this._count++;
+        this._lastTime = window.performance.now();
+
+        return this._texture;
+    }
+});
+
+/**
+ * Is the texture locked ?
+ * @name  FORGE.MediaTexture#locked
+ * @type {boolean}
+ * @readonly
+ */
+Object.defineProperty(FORGE.MediaTexture.prototype, "locked",
+{
+    /** @this {FORGE.MediaTexture} */
+    get: function()
+    {
+        return this._locked;
+    }
+});
+
+/**
+ * Get the size of the texture
+ * @name  FORGE.MediaTexture#size
+ * @type {number}
+ * @readonly
+ */
+Object.defineProperty(FORGE.MediaTexture.prototype, "size",
+{
+    /** @this {FORGE.MediaTexture} */
+    get: function()
+    {
+        return this._size;
+    }
+});
+
+/**
+ * Get the last time when it was called.
+ * @name  FORGE.MediaTexture#lastTime
+ * @type {number}
+ * @readonly
+ */
+Object.defineProperty(FORGE.MediaTexture.prototype, "lastTime",
+{
+    /** @this {FORGE.MediaTexture} */
+    get: function()
+    {
+        return this._lastTime;
+    }
+});
+
+/**
+ * Get the number of times it was called.
+ * @name  FORGE.MediaTexture#count
+ * @type {number}
+ * @readonly
+ */
+Object.defineProperty(FORGE.MediaTexture.prototype, "count",
+{
+    /** @this {FORGE.MediaTexture} */
+    get: function()
+    {
+        return this._count;
+    }
+});
