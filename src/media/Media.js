ef41cadad3739213d267fef520c77c994a138f48
Yann Gilquin <ygilquin@gopro.com>
2017-05-23 09:41:32 +0200

    Media - fire media complete event

--- src/media/Media.js
+++ src/media/Media.js
@@ -143,6 +143,7 @@ FORGE.Media.prototype._parseConfig = function(config)
         if (!source.url)
         {
             this._store = new FORGE.MediaStore(this._viewer, source);
+            this._notifyLoadComplete();
         }
         else if (source.format === FORGE.MediaFormat.EQUIRECTANGULAR ||
             source.format === FORGE.MediaFormat.CUBE ||
