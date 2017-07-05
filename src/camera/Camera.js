79e9a7b984a4efc82b63c661d89cc193cdfac59e
Yann Gilquin <ygilquin@gopro.com>
2017-07-05 15:00:04 +0200

    Fix karma tests (and all background renderers but pyramid)

--- src/camera/Camera.js
+++ src/camera/Camera.js
@@ -779,6 +779,7 @@ FORGE.Camera.prototype._getYawBoundaries = function()
 
     // Check first if background renderer exposes its own limits for the current media
     if (this._viewer.renderer.backgroundRenderer !== null &&
+        typeof this._viewer.renderer.backgroundRenderer.limits !== "undefined" &&
         this._viewer.renderer.backgroundRenderer.limits !== null &&
         typeof this._viewer.renderer.backgroundRenderer.limits.yaw !== "undefined")
     {
@@ -878,6 +879,7 @@ FORGE.Camera.prototype._getPitchBoundaries = function()
 
     // Check first if background renderer exposes its own limits for the current media
     if (this._viewer.renderer.backgroundRenderer !== null &&
+        typeof this._viewer.renderer.backgroundRenderer.limits !== "undefined" &&
         this._viewer.renderer.backgroundRenderer.limits !== null &&
         typeof this._viewer.renderer.backgroundRenderer.limits.pitch !== "undefined")
     {
