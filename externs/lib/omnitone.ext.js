5416cb39285deb2ba7d720c5e23c5c63b7b5fdf8
Adrien Berthet <aberthet@gopro.com>
2017-10-06 16:18:32 +0200

    Add missing externs for omnitone library.

--- externs/lib/omnitone.ext.js
+++ externs/lib/omnitone.ext.js
@@ -57,3 +57,57 @@ FOADecoder.prototype.initialize = function() {};
  * @const
  */
 Omnitone.FOADecoder = FOADecoder;
+
+/**
+ * Create a singleton FOARenderer instance.
+ * @param {AudioContext} context - Associated AudioContext.
+ * @param {FOARendererOptions} options - Options for FOA decoder.
+ * @return {FOARenderer}
+ */
+Omnitone.createFOARenderer = function (context, options) {};
+
+/**
+ * @typedef {{channelMap:(Array|undefined), hrirPathList:(Array|undefined), renderingMode:(string|undefined)}}
+ * @name FOARendererOptions
+ * @property {Array} channelMap - Custom channel routing map.
+ * @property {Array} hrirPathList - A list of paths to HRIR files.
+ * @property {RenderingMode} renderingMode - Rendering mode.
+ */
+var FOARendererOptions;
+
+/**
+ * Omnitone FOA renderer class. Uses the optimized convolution technique.
+ * @constructor
+ * @param {AudioContext} context - Associated AudioContext.
+ * @param {FOARendererOptions} options
+ * @return {!FOARenderer}
+ */
+function FOARenderer(context, options) {};
+
+/**
+ * @const
+ */
+Omnitone.FOARenderer = FOARenderer;
+/**
+ * @param  {Object} cameraMatrix - The Matrix4 object of the Three.js camera.
+ */
+FOARenderer.prototype.setRotationMatrixFromCamera = function(cameraMatrix) {};
+
+/**
+ * @param {string} mode - Decoding mode.
+ * When the mode is 'bypass' the decoder is disabled and bypass the input stream to the output.
+ * Setting the mode to 'ambisonic' activates the decoder.
+ * When the mode is 'off', all the processing is completely turned off saving the CPU power.
+ */
+FOARenderer.prototype.setMode = function(mode) {};
+
+/**
+ *
+ */
+FOARenderer.prototype.initialize = function() {};
+
+/** @type {GainNode} */
+FOARenderer.prototype.input;
+
+/** @type {GainNode} */
+FOARenderer.prototype.output;
