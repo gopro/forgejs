fc0d6fb67dbdf6bad6f0718192ca55bd9a6093fe
Benjamin Baudel <bbaudel@gopro.com>
2017-12-04 14:54:58 +0100

    Add a check on BackgroundRenderer quality change handler for video media

--- src/render/RenderManager.js
+++ src/render/RenderManager.js
@@ -432,7 +432,10 @@ FORGE.RenderManager.prototype._mediaQualityChangeHandler = function(event)
 {
     this.log("Media quality has changed");
 
-    this._backgroundRenderer.displayObject = event.emitter;
+    if (this._backgroundRenderer !== null)
+    {
+        this._backgroundRenderer.displayObject = event.emitter;
+    }
 };
 
 /**
