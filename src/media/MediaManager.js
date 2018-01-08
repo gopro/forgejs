d1c921a3cf66d9e8f3d44f2d65c13fca6a315c79
Raphaël Roux <rroux@gopro.com>
2018-01-08 16:28:46 +0100

    Add doc to the media manager

--- src/media/MediaManager.js
+++ src/media/MediaManager.js
@@ -28,6 +28,11 @@ FORGE.MediaManager = function(viewer)
 FORGE.MediaManager.prototype = Object.create(FORGE.BaseObject.prototype);
 FORGE.MediaManager.prototype.constructor = FORGE.MediaManager;
 
+/**
+ * Update the media
+ * @method FORGE.MediaManager#add
+ * @param {SceneMediaConfig} config - The config of the media to add.
+ */
 FORGE.MediaManager.prototype.add = function(config)
 {
     var media = null;
@@ -71,6 +76,10 @@ FORGE.MediaManager.prototype.add = function(config)
     return media.uid;
 };
 
+/**
+ * Update the media
+ * @method FORGE.MediaManager#update
+ */
 FORGE.MediaManager.prototype.update = function()
 {
     var media;
@@ -86,14 +95,39 @@ FORGE.MediaManager.prototype.update = function()
     }
 };
 
+/**
+ * Load a media by its uid
+ * @method FORGE.MediaManager#load
+ * @param  {string} uid - The uid of the media to load
+ */
 FORGE.MediaManager.prototype.load = function(uid)
 {
     var media = FORGE.UID.get(uid);
     media.load();
 };
 
+/**
+ * Unload a media by its uid
+ * @method FORGE.MediaManager#unload
+ * @param  {string} uid - The uid of the media to unload
+ */
 FORGE.MediaManager.prototype.unload = function(uid)
 {
     var media = FORGE.UID.get(uid);
     media.unload();
 };
+
+/**
+ * Get all the media
+ * @name FORGE.MediaManager#media
+ * @type {FORGE.MediaManager}
+ * @readonly
+ */
+Object.defineProperty(FORGE.MediaManager.prototype, "all",
+{
+    /** @this {FORGE.MediaManager} */
+    get: function()
+    {
+        return this._media;
+    }
+});
