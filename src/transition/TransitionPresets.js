5e1f28ca6ed1097f1b129571a77000190457bb0a
Raphaël Roux <rroux@gopro.com>
2018-03-12 10:20:35 +0100

    Rework TransitionPreset syntax

--- src/transition/TransitionPresets.js
+++ src/transition/TransitionPresets.js
@@ -6,39 +6,94 @@ FORGE.TransitionPresets = {};
 
 /**
  * @name FORGE.TransitionPresets.NONE
- * @type {LayoutConfig}
+ * @type {TransitionConfig}
  * @const
  */
 FORGE.TransitionPresets.NONE =
 {
     uid: "forge-transition-preset-none",
-    name: "None",
-    type: null,
-    duration: 0
+
+    background:
+    {
+        type: null,
+        easing: null,
+        duration: 0,
+    },
+
+    screen:
+    {
+        material: { type: "basic" },
+        easing: null,
+        duration: 0
+    }
 };
 
 /**
  * @name FORGE.TransitionPresets.SLIDE
- * @type {LayoutConfig}
+ * @type {TransitionConfig}
  * @const
  */
 FORGE.TransitionPresets.SLIDE =
 {
     uid: "forge-transition-preset-slide",
-    name: "Screen slide",
-    type: "screen",
-    duration: 500
+
+    background:
+    {
+        type: null,
+        easing: null,
+        duration: 0
+    },
+
+    screen:
+    {
+        material: { type: "slide" },
+        easing: FORGE.Easing.LINEAR,
+        duration: 2000
+    }
+};
+
+/**
+ * @name FORGE.TransitionPresets.BLEND
+ * @type {TransitionConfig}
+ * @const
+ */
+FORGE.TransitionPresets.BLEND =
+{
+    uid: "forge-transition-preset-slide",
+
+    background:
+    {
+        type: null,
+        easing: null,
+        duration: 0
+    },
+
+    screen:
+    {
+        material: { type: "blend" },
+        easing: FORGE.Easing.LINEAR,
+        duration: 2000
+    }
 };
 
 /**
  * @name FORGE.TransitionPresets.SPHERICAL
- * @type {LayoutConfig}
+ * @type {TransitionConfig}
  * @const
  */
 FORGE.TransitionPresets.SPHERICAL =
 {
-    uid: "forge-transition-preset-spherical",
-    name: "Spherical",
-    type: "spherical",
-    duration: 500
+    background:
+    {
+        type: "@todo to be defined",
+        easing: FORGE.Easing.LINEAR,
+        duration: 2000
+    },
+
+    screen:
+    {
+        material: { type: "basic" },
+        easing: null,
+        duration: 0
+    }
 };
