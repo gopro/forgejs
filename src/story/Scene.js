289ababdf63dbc7435a90949bd9420c40a396f13
Raphaël Roux <rroux@gopro.com>
2018-03-14 17:23:27 +0100

    Add a getter for mediaUid on FORGE.Scene

--- src/story/Scene.js
+++ src/story/Scene.js
@@ -782,6 +782,21 @@ Object.defineProperty(FORGE.Scene.prototype, "thumbnails",
 });
 
 /**
+ * Get the scene media uid.
+ * @name  FORGE.Scene#mediaUid
+ * @readonly
+ * @type {string}
+ */
+Object.defineProperty(FORGE.Scene.prototype, "mediaUid",
+{
+    /** @this {FORGE.Scene} */
+    get: function()
+    {
+        return this._mediaUid;
+    }
+});
+
+/**
  * Get the scene media.
  * @name  FORGE.Scene#media
  * @readonly
